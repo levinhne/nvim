39a6c3358abd207d53286aaa9e8a681a5153d7c7
vinhlq <vinhlq@smartos.com>
2024-03-20 11:26:36 +0700

    .

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,12 +2,19 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "github_dark",
+  transparency = true,
+  telescope = { style = "bordered" },
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "block",
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -33,3 +33,9 @@ lspconfig.gopls.setup({
 		},
 	},
 })
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
