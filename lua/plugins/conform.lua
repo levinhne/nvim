15ad3ade63767abb3dd0772f33652d3bc0b9b024
vinhlq <vinhlq@smartos.com>
2024-03-20 11:26:45 +0700

    .

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    python = { "isort", "black" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+local plugins = {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    config = function()
+      require("conform").setup(options)
+    end,
+  },
+}
+
+return plugins
