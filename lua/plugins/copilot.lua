872f88a2d47afbd0580147cd21ac1dd420d523b3
Vinh Quang Le <vinhlq@smartosc.com>
2024-05-30 09:46:17 +0700

    add visual multi

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,12 @@ local plugins = {
     "github/copilot.vim",
     event = "InsertEnter",
     config = function()
+      vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
+      vim.g.copilot_no_tab_map = true
       vim.g.copilot_enabled = true
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_filetypes = {
---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,9 @@
+local plugins = {
+  {
+    "mg979/vim-visual-multi",
+    config = function() end,
+    event = "CursorHold",
+  },
+}
+
+return plugins
