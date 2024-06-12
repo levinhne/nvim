49abc9ed0b70d7fa49e453d069b5589642b54396
Vinh Quang Le <vinhlq@smartosc.com>
2024-06-12 10:05:22 +0700

    update nvim

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,20 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+local g = {
 
-vim.opt.mouse = "v"
-vim.opt.swapfile = false
+}
+
+local opts = {
+  relativenumber = true,
+  mouse = "v",
+  swapfile = false
+}
+
+for k, v in pairs(g) do
+  vim.g[k] = v
+end
+
+for k, v in pairs(opt) do
+  vim.opt[k] = v
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,22 @@
 return {
+  { import = "plugins.override.treesitter" },
+  { import = "plugins.override.neoscroll" },
+  { import = "plugins.override.wilder" },
+  { import = "plugins.override.zoxide" },
+  { import = "plugins.override.visual-multi" },
+  { import = "plugins.override.flash" },
+  --
+  { import = "plugins.override.mason" },
+  { import = "plugins.override.lspconfig" },
+  { import = "plugins.override.conform" },
+  --
+  { import = "plugins.override.lazygit" },
+  { import = "plugins.override.diffview" },
+  --
   { import = "plugins.override.go" },
   { import = "plugins.override.fzf" },
   { import = "plugins.override.helm" },
-  { import = "plugins.override.flash" },
-  { import = "plugins.override.mason" },
-  { import = "plugins.override.wilder" },
-  { import = "plugins.override.zoxide" },
+  -- 
   { import = "plugins.override.codeium" },
   { import = "plugins.override.copilot" },
-  { import = "plugins.override.lazygit" },
-  { import = "plugins.override.conform" },
-  { import = "plugins.override.diffview" },
-  { import = "plugins.override.lspconfig" },
-  { import = "plugins.override.neoscroll" },
-  { import = "plugins.override.treesitter" },
-  { import = "plugins.override.visual-multi" },
 }
